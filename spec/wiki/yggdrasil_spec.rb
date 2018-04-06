257a208192e6ed461a09a26daca5d0136d862df2
alex0112 <alarsen0112@gmail.com>
2018-04-05 22:09:22 -0600

    Removed test stub

--- spec/wiki/yggdrasil_spec.rb
+++ spec/wiki/yggdrasil_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Wiki::Yggdrasil do
   it "has a version number" do
     expect(Wiki::Yggdrasil::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
