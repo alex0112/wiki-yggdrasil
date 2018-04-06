3dbc2d73b2233e4974cbe879b0e46968fad2ea8b
alex0112 <alarsen0112@gmail.com>
2018-04-05 22:09:56 -0600

    Added article class and tests/stubs

---
+++ lib/wiki/article.rb
@@ -0,0 +1,15 @@
+module Wiki::Yggdrasil
+  class Article
+    attr_reader :uri
+
+    def initialize(uri:)
+      raise ArgumentError unless Wiki::Yggdrasil::Article.is_valid_wiki_article?(uri: uri)
+    end
+
+    def self.is_valid_wiki_article?(uri:)
+      ## Is this URI a wikipedia article?
+      uri =~ /.*wikipedia\.org\/wiki\/.+/ ? true : false
+    end
+
+  end
+end
---
+++ spec/wiki/article_spec.rb
@@ -0,0 +1,52 @@
+describe Wiki::Yggdrasil::Article do
+
+  before(:all) do
+    @article = Wiki::Yggdrasil::Article.new(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')
+  end
+
+  describe '#new' do
+    context 'Given a valid wikipedia article' do
+      it 'returns a Wiki::Yggdrasil::Article object' do
+        expect(Wiki::Yggdrasil::Article.new(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')).to be_a Wiki::Yggdrasil::Article
+      end
+    end
+
+    context 'Given no uri' do
+      it 'raises an ArgumentError' do
+        expect { Wiki::Yggdrasil::Article.new() }.to raise_error ArgumentError
+      end
+    end
+    
+    context 'Given a non-valid wikipedia article' do
+      it 'raises an argument error' do
+        expect { Wiki::Yggdrasil::Article.new() }.to raise_error ArgumentError
+      end
+    end
+  end
+
+  describe '#is_valid_wiki_article?' do
+    context 'Given an invalid uri' do
+      it 'returns false' do
+        expect(Wiki::Yggdrasil::Article.is_valid_wiki_article?(uri: 'https://www.google.com/')).to be false
+      end
+    end
+    context 'Given a valid uri' do
+      it 'returns true' do
+        expect(Wiki::Yggdrasil::Article.is_valid_wiki_article?(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')).to be true
+      end
+    end
+  end
+  
+  xdescribe '.summary' do
+    it 'only returns the summary section' do
+    end
+  end
+
+  xdescribe '.summary_links' do
+    it 'returns a list' do
+    end
+
+    it 'returns a list of valid wikipedia URIs' do
+    end
+  end
+end
