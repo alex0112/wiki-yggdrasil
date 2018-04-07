6c5f320d1eeecb8011e23d5ad67acc53f65114f2
alex0112 <alarsen0112@gmail.com>
2018-04-07 01:31:54 -0600

    Getters and setters for :uri and :summary in Article

--- lib/wiki/article.rb
+++ lib/wiki/article.rb
@@ -1,11 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 module Wiki::Yggdrasil
+  
   class Article
     attr_reader :uri
-
+    attr_reader :summary
+    
     def initialize(uri:)
       raise ArgumentError unless Wiki::Yggdrasil::Article.is_valid_wiki_article?(uri: uri)
+      @uri     = uri
+      @summary = nil
     end
 
     def self.is_valid_wiki_article?(uri:)
