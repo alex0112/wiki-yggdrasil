ee56b7a16381e3e905a8a00a725992b5762821e5
alex0112 <alarsen0112@gmail.com>
2018-04-09 18:33:20 -0600

    #initialize method and tests for Wiki::Yggdrasil::Yggdrasil

--- lib/wiki/yggdrasil.rb
+++ lib/wiki/yggdrasil.rb
@@ -3,5 +3,15 @@ require "wiki/yggdrasil/version"
 module Wiki
   module Yggdrasil
     require 'wiki/article'
+
+    class Yggdrasil
+      attr_reader :root
+
+      def initialize(uri:)
+        @root = Wiki::Yggdrasil::Article.new(uri: uri)
+      end
+
+    end
+
   end
 end
--- spec/wiki/yggdrasil_spec.rb
+++ spec/wiki/yggdrasil_spec.rb
@@ -1,5 +1,35 @@
-RSpec.describe Wiki::Yggdrasil do
+describe Wiki::Yggdrasil do
+  before(:all) do
+    @tree = Wiki::Yggdrasil::Yggdrasil.new(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')
+  end
+
   it "has a version number" do
     expect(Wiki::Yggdrasil::VERSION).not_to be nil
   end
+
+  
+  describe '#new' do
+    context 'Given no uri' do
+      it 'raises an ArgumentError' do
+        expect { Wiki::Yggdrasil::Yggdrasil.new() }.to raise_error ArgumentError
+      end
+    end
+    
+    context 'Given a valid wikipedia article' do
+      it 'returns a Wiki::Yggdrasil object' do
+           expect(Wiki::Yggdrasil::Yggdrasil.new(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')).to be_a Wiki::Yggdrasil::Yggdrasil
+      end
+    end
+
+    context 'Given a non-valid wikipedia article' do
+      it 'raises an ArgumentError' do
+        expect { Wiki::Yggdrasil::Yggdrasil.new(uri: 'https://www.google.com') }.to raise_error ArgumentError
+      end
+    end
+
+    it 'has a Wiki::Yggdrasil::Article as its @root attribute' do
+      expect(@tree.root).to be_a Wiki::Yggdrasil::Article
+    end
+  end
+
 end
