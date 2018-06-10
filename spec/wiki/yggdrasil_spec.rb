404da57a5e86b956cfbb39491b0693cf144c5eac
alex0112 <alarsen0112@gmail.com>
2018-06-10 00:58:12 -0600

    Test greater depth for Yggdrasil.children

--- spec/wiki/yggdrasil_spec.rb
+++ spec/wiki/yggdrasil_spec.rb
@@ -1,6 +1,8 @@
 describe Wiki::Yggdrasil do
   before(:all) do
     @tree = Wiki::Yggdrasil::Yggdrasil.new(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')
+    @tree_2 = Wiki::Yggdrasil::Yggdrasil.new(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')
+    @tree_3 = Wiki::Yggdrasil::Yggdrasil.new(uri: 'https://en.wikipedia.org/wiki/Yggdrasil')
   end
 
   it "has a version number" do
@@ -34,10 +36,20 @@ describe Wiki::Yggdrasil do
     end
   end
 
-  describe '.children' do
-    it 'returns a hash' do
+  describe '.children' do  ## TODO:  Cleanup and implement mocking
+    it 'returns a hash for a tree with a depth of 1' do
       children = @tree.children(depth: 1)
       expect(children).to be_a Hash
+    end
+
+    it 'returns a hash for a tree with a depth of 2' do
+      children_2 = @tree_2.children(depth: 2)
+      expect(children_2).to be_a Hash
+    end
+
+    it 'returns a hash for a tree with a depth of 3' do
+      children_3 = @tree_3.children(depth: 3)
+      expect(children_3).to be_a Hash
     end
   end
   
